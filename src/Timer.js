a1430688040acbdceff95a1e370a4c12bb52fec2
katerina-shche <asti.katerina@gmail.com>
2023-05-29 13:12:57 +0200

    change session to Break

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,7 @@ import './Timer.css'
 
 export default function Timer({ title, minutes, onReset, onSwitchToBreak, onSwitchToSession }) {
     const prevMinutes = useRef(minutes)
+    const prevTitle = useRef(title)
     const [isRunning, setIsRunning] = useState(false)
     const secondsLeft = useRef(minutes * 60)
     const displayTimeLeft = (anySeconds) => {
@@ -21,8 +22,13 @@ export default function Timer({ title, minutes, onReset, onSwitchToBreak, onSwit
         if (prevMinutes.current !== minutes) {
             secondsLeft.current = minutes * 60
             setTimeString(displayTimeLeft(secondsLeft.current))
-            setIsRunning(false)
             prevMinutes.current = minutes
+        }
+        if (prevTitle.current !== title) {
+            secondsLeft.current = minutes * 60
+            setTimeString(displayTimeLeft(secondsLeft.current))
+            prevMinutes.current = minutes
+            prevTitle.current = title
         }
         // if no new minutes then everething else goes as usual
         let timer = null
@@ -33,18 +39,19 @@ export default function Timer({ title, minutes, onReset, onSwitchToBreak, onSwit
                 timer = setInterval(() => {
                 // check if we should stop it!
                  if (secondsLeft.current <= 0) {
+                    setTimeString('00:00')
+                    secondsLeft.current = NaN
                     if (title === "Session") {
                         onSwitchToBreak()
                     } else {
                         onSwitchToSession()
                     }
-                    secondsLeft.current = minutes * 60
-                    setTimeString(displayTimeLeft(secondsLeft.current))
-                    setIsRunning(true)
+                    
                     const audio = document.querySelector('audio')
                     audio.currentTime = 0; // rewind to the start
                     audio.play();
-                    return
+                    secondsLeft.current = minutes * 60
+
                     } 
                 secondsLeft.current = Math.round((then - Date.now()) / 1000)
                 setTimeString(displayTimeLeft(secondsLeft.current))  
